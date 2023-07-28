a8638de9d667a8a035a88d96f14e42ea87e06cb5
akash <akashcsemu@gmail.com>
2023-07-28 16:45:19 +0600

    feat: Counter, Blog and Skill section added

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,28 +1,18 @@
-import { useState } from 'react';
-import { useLocation, NavLink } from 'react-router-dom'
-import { FaLinkedin, FaGithub, FaYoutube} from 'react-icons/fa6'
-import { PiDevToLogoLight } from 'react-icons/pi'
+import { Blog, Counter, Profile,Skills } from '../Components'
+
 const About = ()=>{
-    const [designation, setDesignatioin] = useState('Software Engineer')
-    const [organizationName, setOrganizationName]  = useState('Gain Solutions')
     return (
         <>
             <div className='main-container'>
-                <div className='flex flex-col gap-y-5 md:flex-row items-center justify-center gap-x-20 my-10'>
-                    <div>
-                        <img src="../../src/assets/image/profile.jpg" alt="profile" className='h-60 mx-auto rounded-full ring-4 ring-[#ffffff]'/>
+                <Profile/>
+                <div className='flex flex-col md:flex-row md:items-start  md:justify-center gap-5'>
+                    <div className='md:w-[30%] flex flex-col'>
+                        <Counter/>
+                        <Skills/>
                     </div>
-                    <div className='flex flex-col justify-item-stretch'>
-                        <p><span className='text-[#00DF9A]'> as </span><span className='font-bold text-3xl'>Akash Karmokar</span></p>
-                        <p>{designation} @ {organizationName}</p>
-                        <div className='flex flex-row gap-3 justify-center mt-2 md:justify-start'>
-                            <NavLink to={"https://www.linkedin.com/in/akashcsemu/"} target='_blank'><FaLinkedin className='text-xl'/></NavLink>
-                            <NavLink to={"https://github.com/Akashkarmokar"} target='_blank'><FaGithub className='text-xl'/></NavLink>
-                            <NavLink to={"https://dev.to/akashcsemu"} target='_blank'><PiDevToLogoLight className='text-xl'/></NavLink>
-                            <NavLink to={"https://www.youtube.com/@OpenTerminal108"} target='_blank'><FaYoutube className='text-xl'/></NavLink>
-                        </div>
+                    <div className="flex flex-col md:w-[70%] ">
+                        {Array.from([1,2,3,4,5,6,7,8,9,0].map((val)=> (<Blog key={val}/>)))}
                     </div>
-
                 </div>
             </div>
         </>
