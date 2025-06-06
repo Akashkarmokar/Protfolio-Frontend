79f44864e7a461d5fbd27b49389b711d08cc3ccc
ak <akashcsemu@gmail.com>
2025-06-06 19:30:18 +0600

    fix: Admin Input

--- src/Components/Dashboard/ProfileSetup.jsx
+++ src/Components/Dashboard/ProfileSetup.jsx
@@ -6,21 +6,21 @@ const ProfileSetup = () => {
             <div className="flex flex-col items-center justify-center">
                 <h1 className="text-2xl">Profile Setup</h1>
                 <p className="text-lg">This is a placeholder for the profile setup content.</p>
-                <div className="bg-[#3e403f] w-[100%] space-y-4 rounded-md border p-4">
+                <div className="bg-[#3e403f] w-full space-y-4 rounded-md border p-4">
                     <div>
-                        <input className="bg-[#3e403f]" type="text" placeholder="Name"/>
+                        <input className="bg-[#3e403f] bg-transparent border rounded-md w-full" type="text" placeholder="Name"/>
                     </div>
                     <div>
-                        <input className="bg-[#3e403f]" type="text" placeholder="Current Designation"/>
+                        <input className="bg-[#3e403f] bg-transparent border rounded-md w-full" type="text" placeholder="Current Designation"/>
                     </div>
                     <div>
-                        <input className="bg-[#3e403f]" type="text" placeholder="Bio"/>
+                        <input className="bg-[#3e403f] bg-transparent border rounded-md w-full" type="text" placeholder="Bio"/>
                     </div>
                     <div>
-                        <input className="bg-[#3e403f]" type="text" placeholder="Moto"/>
+                        <input className="bg-[#3e403f] bg-transparent border rounded-md w-full" type="text" placeholder="Moto"/>
                     </div>
                     <div>
-                        <input className="bg-[#3e403f]" type="submit" placeholder="Moto" onClick={()=> console.log("Hello world")}/>
+                        <input className="bg-[#3e403f] bg-transparent border rounded-md w-full" type="submit" placeholder="Moto" onClick={()=> console.log("Hello world")}/>
                     </div>
 
                 </div>
